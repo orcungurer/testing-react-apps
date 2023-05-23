4fb01c85bdb708d40e40b495c24107e0cabc73a4
orcungurer <orcungurer@hotmail.com>
2023-05-23 12:54:17 +0300

    committed first test for Greeting

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react"; // 2, 4
+import Greeting from "./Greeting"; // 2
+
+// 1
+test("renders Hello World! as text", () => {
+  // 2, arrange
+  render(<Greeting />);
+
+  // 3, act
+  // nothing
+
+  // 4, assert
+  const helloWorldElement = screen.getByText("Hello World!");
+
+  expect(helloWorldElement).toBeInTheDocument(); // 5
+});
