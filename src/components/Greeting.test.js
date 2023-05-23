dad8717fdd6910e56f7a10c89b92beac21ca14be
orcungurer <orcungurer@hotmail.com>
2023-05-23 15:39:49 +0300

    grouping our tests using test suites.

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,16 +1,19 @@
 import { render, screen } from "@testing-library/react"; // 2, 4
 import Greeting from "./Greeting"; // 2
 
-// 1
-test("renders Hello World! as text", () => {
-  // 2, arrange
-  render(<Greeting />);
+// 6
+describe("Greeting Component", () => {
+  // 1
+  test("renders Hello World! as text", () => {
+    // 2, arrange
+    render(<Greeting />);
 
-  // 3, act
-  // nothing
+    // 3, act
+    // nothing
 
-  // 4, assert
-  const helloWorldElement = screen.getByText("Hello World!");
+    // 4, assert
+    const helloWorldElement = screen.getByText("Hello World!");
 
-  expect(helloWorldElement).toBeInTheDocument(); // 5
+    expect(helloWorldElement).toBeInTheDocument(); // 5
+  });
 });
